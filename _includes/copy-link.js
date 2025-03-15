d1b0520f874016cc20d8a382c6cd641bc9277a11
Melanie McDonough <melbamorph@users.noreply.github.com>
2025-03-14 20:43:46 -0400

    Delete _includes directory

--- _includes/copy-link.js
+++
@@ -1,6 +0,0 @@
-function copyToClipboard(id) {
-    const fullUrl = window.location.origin + window.location.pathname + id;
-    navigator.clipboard.writeText(fullUrl).then(() => {
-        alert('Link copied: ' + fullUrl);
-    });
-}
