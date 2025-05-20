14330f3c3f448eac9fae296274e446095f5e9cc1
Melanie McDonough <melbamorph@users.noreply.github.com>
2025-05-19 20:33:32 -0400

    Improve copyToClipboard error handling (#3)

--- assets/js/copy-link.js
+++ assets/js/copy-link.js
@@ -1,6 +1,15 @@
 function copyToClipboard(id) {
     const fullUrl = window.location.origin + window.location.pathname + id;
-    navigator.clipboard.writeText(fullUrl).then(() => {
-        alert('Link copied: ' + fullUrl);
-    });
+
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(fullUrl)
+            .then(() => {
+                alert('Link copied: ' + fullUrl);
+            })
+            .catch(() => {
+                alert('Failed to copy link to clipboard.');
+            });
+    } else {
+        alert('Clipboard API not available.');
+    }
 }
